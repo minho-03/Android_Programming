39474d458d7449f65804f8de6776eae2144b5629
minho <pmh9038@naver.com>
2026-05-14 16:02:19 +0900

    맛집 지도 기능 구현 완료

--- CookMap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ CookMap/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -1,80 +1,107 @@
 package com.cookandroid.cookmap;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
-    SupportMapFragment mapFrag;
-    GroundOverlayOptions videoMark;
+    MapFragment mapFrag;
+    GroundOverlayOptions placeMark;
+    Button btnPrev, btnNext;
+    List<String> lines = new ArrayList<>();
+    int placeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("구글 지도 활용");
-
-        mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        setTitle("경기도 으뜸 맛집");
+        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        readCSV();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String line = lines.get(placeCount++);
+                if (placeCount > lines.size() - 1)
+                    placeCount = 0;
+                String[] tokens = line.split(",");
+                double lat = Double.parseDouble(tokens[0]);
+                double lon = Double.parseDouble(tokens[1]);
+                String restName = tokens[2];
+
+                LatLng point;
+                point = new LatLng(lat, lon);
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
+                placeMark = new GroundOverlayOptions().image(
+                        BitmapDescriptorFactory.fromResource(R.drawable.food)).position(point, 500f, 500f);
+                gMap.addGroundOverlay(placeMark);
+                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
+            }
+        });
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String line = lines.get(placeCount--);
+                if (placeCount < 0)
+                    placeCount = lines.size()-1;
+                String[] tokens = line.split(",");
+                double lat = Double.parseDouble(tokens[0]);
+                double lon = Double.parseDouble(tokens[1]);
+                String restName = tokens[2];
+
+                LatLng point;
+                point = new LatLng(lat, lon);
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
+                placeMark = new GroundOverlayOptions().image(
+                        BitmapDescriptorFactory.fromResource(R.drawable.food)).position(point, 500f, 500f);
+                gMap.addGroundOverlay(placeMark);
+                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         gMap = map;
-        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
         gMap.getUiSettings().setZoomControlsEnabled(true);
+    }
 
-        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
-            @Override
-            public void onMapClick(LatLng point){
-                videoMark = new GroundOverlayOptions()
-                        .image(BitmapDescriptorFactory.fromResource(android.R.drawable.presence_video_busy))
-                        .position(point, 400f, 400f);
-                gMap.addGroundOverlay(videoMark);
+    public void readCSV() {
+        InputStream inputStream = getResources().openRawResource(R.raw.good_place);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String line;
+            line = reader.readLine(); // 첫 제목행을 읽어서 버림
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
             }
-        });
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu){
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, 1, 0, "위성지도");
-        menu.add(0, 2, 0, "일반지도");
-        android.view.SubMenu sMenu = menu.addSubMenu("유명장소 바로가기 >>");
-        sMenu.add(0,4,0,"연희직업전문학교");
-        sMenu.add(0,5,0, "집");
-        sMenu.add(0,6,0,"맥도날드");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch(item.getItemId()){
-            case 1:
-                gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                return true;
-            case 2:
-                gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                return true;
-            case 4:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.542886, 126.677220), 18));
-                return true;
-            case 5:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.449699, 126.645448), 18));
-                return true;
-            case 6:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.542727, 126.676167), 18));
-                return true;
-        }
-        return false;
+            reader.close();
+        } catch (Exception e) {}
     }
 }
